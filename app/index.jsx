78f6b7fd815f36a4ac2e924db46b98359a92e002
Luan Au <luanau@gmail.com>
2016-02-17 14:01:36 +0000

    Barebone react view created

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// 
+// var component = require('./component');
+// var app = document.createElement('div');
+//
+//
+// document.body.appendChild(app);
+//
+// app.appendChild(component());
